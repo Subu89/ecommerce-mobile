ffc8f02f67bc4fa97aa2c54bef56f719e1d59973
Subu89 <sudipmhnt@gmail.com>
2024-01-11 22:12:42 +0530

    changed package.json

--- client/redux/features/auth/userActions.js
+++ client/redux/features/auth/userActions.js
@@ -32,4 +32,39 @@ export const login = (email, password) => async (dispatch) => {
 };
 
 // register action
-export const register = (formDat)
+export const register = (formData) => async (dispatch) => {
+    try {
+        dispatch({
+            type: "registerRequest"
+        });
+        // hitapi register
+        const { data } = await axios.post(`${server}/user/register`, formData, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        dispatch({
+            type: "registerSuccess",
+            payload: data.message,
+        });
+    } catch (error) {
+        console.log(error);
+        dispatch({
+            type: "registerFail",
+            payload: error.response.data.message,
+        });
+    }
+};
+
+// GET USER DATA ACTION
+export const getUserData = () => async (dispatch) => {
+    try {
+        dispatch({
+            type: "getUserDataRequest",
+        });
+        // hitting node login api request
+        
+    } catch (error) {
+        
+    }
+}

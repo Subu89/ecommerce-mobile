cc744988a8b05ce14f82f7d665d6a3ac20b5afec
Subu89 <sudipmhnt@gmail.com>
2024-01-12 22:11:51 +0530

    modified route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -19,6 +19,7 @@ const limiter = rateLimit({
     limit: 100, // Limit each IP to 100 requests per window (here, per 15 minutes).
     standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    validate: {xForwardedForHeader: false},
     // store: ... , // Use an external store for consistency accross multiple server instances.
 });
 
